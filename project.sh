d43bd2369501065b1f00e8629954b3242fc06a11
KowsiKohi <114602385+KowsiKohi@users.noreply.github.com>
2022-11-16 21:45:09 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hello World, hey !"
 # rsync script
-chown jenkins:jenkins /tmp
 mkdir -p /tmp/home
-rsync -avh --exclude="*.bak" /home /tmp/home
+chown jenkins:jenkins /tmp/home
+touch /tmp/home/test1.txt
